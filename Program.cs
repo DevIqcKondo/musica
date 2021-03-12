bad8fc347c97c712b15e1fe6937c378b3b7160ae
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-03-12 15:29:18 -0300

    Quebra de linha. Alteração no título

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ namespace musica
         static void Main(string[] args)
         {
             Console.ForegroundColor=ConsoleColor.DarkCyan;
-            Console.WriteLine("One Last Breath");
+            Console.WriteLine("\\---------------\\");
+            Console.WriteLine("\\One Last Breath\\");
+             Console.WriteLine("\\---------------\\");
+            Console.WriteLine();
             Console.ForegroundColor=ConsoleColor.Yellow;
             Console.WriteLine("Please, come now, I think I'm falling");
             Console.WriteLine("I'm holding on to all I think is safe");
@@ -15,6 +18,8 @@ namespace musica
             Console.WriteLine("And I'm trying to escape, I yelled back when I heard thunder");
             Console.WriteLine("But I'm down to one last breath");
             Console.WriteLine("And with it, let me say, Let me say");
+            Console.WriteLine();
+
             Console.ForegroundColor=ConsoleColor.Red;
             Console.WriteLine("Hold me now, I'm six feet from the edge");
             Console.WriteLine("and I'm thinking maybe six feet ain't so far down");
@@ -24,13 +29,19 @@ namespace musica
             Console.WriteLine("But I'm down to one last breath and with it, let me say");
             Console.WriteLine("Hold me now i'm six feet from the edge and I'm thinking");
             Console.WriteLine("Maybe six feet ain't so far down,i'm so far down");
+            Console.WriteLine();
+
             Console.ForegroundColor=ConsoleColor.Green;
             Console.WriteLine("Sad eyes follow me");
             Console.WriteLine("But I still believe there's something left for me");
             Console.WriteLine("So, please, come stay with me 'Cause");  
-            Console.WriteLine("I still believe there's something left for you and me");   
+            Console.WriteLine("I still believe there's something left for you and me");  
+            Console.WriteLine();
+
             Console.ForegroundColor=ConsoleColor.Blue;
             Console.WriteLine("Hold me now i'm six feet from the edge and I'm thinking");
+            Console.WriteLine();
+            
             Console.ForegroundColor=ConsoleColor.Black;
             Console.WriteLine("Please, come now, I think I'm falling");
             Console.WriteLine("I'm holding on to all I think is safe");                                                           
